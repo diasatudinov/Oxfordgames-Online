a0994d83bf21e3deda93c1539b26ae8dd1f7fbd6
Dias <dias.atudinov@gmail.com>
2025-05-13 16:41:32 +0500

    app setup

---
+++ Oxfordgames Online/Oxfordgames Online/Oxfordgames_OnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oxfordgames_OnlineApp.swift
+//  Oxfordgames Online
+//
+//  Created by Dias Atudinov on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Oxfordgames_OnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
