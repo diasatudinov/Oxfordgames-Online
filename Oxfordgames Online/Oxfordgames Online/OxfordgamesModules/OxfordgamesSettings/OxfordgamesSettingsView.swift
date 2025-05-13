7c8b9638d26766ceebbabdaeff4e860532be92b4
Dias <dias.atudinov@gmail.com>
2025-05-13 17:38:38 +0500

    add settings view

--- Oxfordgames Online/Oxfordgames Online/OxfordgamesModules/OxfordgamesSettings/OxfordgamesSettingsView.swift
+++ Oxfordgames Online/Oxfordgames Online/OxfordgamesModules/OxfordgamesSettings/OxfordgamesSettingsView.swift
@@ -6,13 +6,96 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct OxfordgamesSettingsView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    @ObservedObject var settingsVM: SettingsViewModelSG
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            ZStack {
+                Image(.settingsBgOxfordgames)
+                    .resizable()
+                    .scaledToFit()
+                VStack(spacing: 10) {
+                    
+                    
+                    
+                    VStack {
+                        Image(.soundTextOxfordgames)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: SGDeviceManager.shared.deviceType == .pad ? 60:30)
+                        
+                        Button {
+                            withAnimation {
+                                settingsVM.soundEnabled.toggle()
+                            }
+                        } label: {
+                            
+                            Image(settingsVM.soundEnabled ? .onOxfordgames:.offOxfordgames)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SGDeviceManager.shared.deviceType == .pad ? 80:40)
+                        }
+                    }
+                    
+                    VStack {
+                        Image(.languageTextOxfordgames)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: SGDeviceManager.shared.deviceType == .pad ? 60:30)
+                        
+                        
+                        Image(.enImageOxfordgames)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: SGDeviceManager.shared.deviceType == .pad ? 64:32)
+                    }
+                    
+                }
+                
+                
+            }.frame(height: SGDeviceManager.shared.deviceType == .pad ? 700:366)
+            
+            VStack {
+                HStack {
+                    HStack(alignment: .top) {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                            
+                        } label: {
+                            Image(.backIconOxfordgames)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SGDeviceManager.shared.deviceType == .pad ? 150:75)
+                        }
+                        Spacer()
+                       
+                        CoinBgSG()
+                    }.padding([.horizontal, .top])
+                }
+                Spacer()
+            }
+        }.background(
+            ZStack {
+                Image(.appBgOxfordgames)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+        )
+    }
+    
+    func rateUs() {
+        if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+            SKStoreReviewController.requestReview(in: scene)
+        }
     }
 }
 
 #Preview {
-    OxfordgamesSettingsView()
+    OxfordgamesSettingsView(settingsVM: SettingsViewModelSG())
 }
--- Oxfordgames Online/Oxfordgames Online/OxfordgamesModules/OxfordgamesSettings/SettingsViewModelSG.swift
+++ Oxfordgames Online/Oxfordgames Online/OxfordgamesModules/OxfordgamesSettings/SettingsViewModelSG.swift
@@ -1,7 +1,13 @@
+//
+//  SettingsViewModelSG.swift
+//  Oxfordgames Online
+//
+//  Created by Dias Atudinov on 13.05.2025.
+//
+
+
 import SwiftUI
 
 class SettingsViewModelSG: ObservableObject {
-    @AppStorage("musicEnabled") var musicEnabled: Bool = true
     @AppStorage("soundEnabled") var soundEnabled: Bool = true
-    @AppStorage("vibraEnabled") var vibraEnabled: Bool = true
 }
